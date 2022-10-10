0c2d440e3c7f76f3928b07edace85963a33651e9
AFRIKAN BACKEND DEVELOPER <christopherleon237@gmail.com>
2022-10-10 23:27:05 +0100

    chore: basic django setup

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -124,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT=BASE_DIR/"staticfiles"
+STATICFILES_DIR=[]
+
+MEDIA_URL='/mediafiles/'
+MEDIA_ROOT=BASE_DIR/"mediafiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
